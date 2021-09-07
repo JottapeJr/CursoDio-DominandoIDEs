222aeec3388cc29898b6be5d366dc817571f1501
JottapeJr <jorginho.jpg20@gmail.com>
2021-09-07 18:24:11 -0300

    Adiciona arquivos java ao github(teste)

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+import dio.java.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		
+		/*	int a = 2;
+		int b = 3;
+		System.out.println("Hallo World! " + (a+b));
+	}*/
+	}
+}
